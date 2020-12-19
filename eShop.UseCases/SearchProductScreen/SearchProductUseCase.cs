c8837cfebbb671a9dba9a54cd396a4dfad92c8cd
Chris Roche <chris@wetrack.com>
2020-12-19 16:05:08 +0000

    Add project files.

---
+++ eShop.UseCases/SearchProductScreen/SearchProductUseCase.cs
@@ -0,0 +1,25 @@
+﻿using eShop.CoreBusiness.Models;
+using eShop.UseCases.PluginInterfaces.DataStore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace eShop.UseCases.SearchProductScreen
+{
+    public class SearchProductUseCase : ISearchProductUseCase
+    {
+        private readonly IProductRepository productRepository;
+
+        public SearchProductUseCase(IProductRepository productRepository)
+        {
+            this.productRepository = productRepository;
+        }        
+
+        public IEnumerable<Product> Execute(string filter)
+        {
+            return productRepository.GetProducts(filter);
+        }
+
+
+    }
+}
