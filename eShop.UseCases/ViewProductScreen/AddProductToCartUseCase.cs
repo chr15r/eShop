25c68bda500e86ae8a54d7b628431c46e63ff9a3
Chris Roche <chris@wetrack.com>
2020-12-19 18:07:47 +0000

    Add product to card use case

---
+++ eShop.UseCases/ViewProductScreen/AddProductToCartUseCase.cs
@@ -0,0 +1,27 @@
+﻿using eShop.UseCases.PluginInterfaces.DataStore;
+using eShop.UseCases.PluginInterfaces.UI;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace eShop.UseCases.ViewProductScreen
+{
+    public class AddProductToCartUseCase : IAddProductToCartUseCase
+    {
+        private readonly IShoppingCart shoppingCart;
+        private readonly IProductRepository productRepository;
+
+        public AddProductToCartUseCase(IProductRepository _productRepository, IShoppingCart _shoppingCart)
+        {
+            productRepository = _productRepository;
+            shoppingCart = _shoppingCart;
+        }
+
+
+        public async void Execute(int productId)
+        {
+            var product = productRepository.GetProduct(productId);
+            await shoppingCart.AddProductAsync(product);
+        }
+    }
+}
