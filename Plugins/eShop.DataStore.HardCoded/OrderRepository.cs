b05b57b3d0cb66c34517f431801e115dc8ae5c42
Chris Roche <chris@wetrack.com>
2020-12-23 13:25:57 +0000

    Order repository

---
+++ Plugins/eShop.DataStore.HardCoded/OrderRepository.cs
@@ -0,0 +1,70 @@
+﻿using eShop.CoreBusiness.Models;
+using eShop.UseCases.PluginInterfaces.DataStore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace eShop.DataStore.HardCoded
+{
+    public class OrderRepository : IOrderRepository
+    {
+
+        private Dictionary<int, Order> orders;
+
+        public OrderRepository()
+        {
+            orders = new Dictionary<int, Order>();
+        }
+
+        public int CreateOrder(Order order)
+        {
+            order.OrderId = orders.Count + 1;
+            order.UniqueId = Guid.NewGuid().ToString();
+            orders.Add(order.OrderId.Value, order);
+            return order.OrderId.Value;
+        }
+
+        public IEnumerable<OrderLineItem> GetLineItemsByOrderId(int orderId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Order GetOrder(int Id)
+        {
+            return orders[Id];
+        }
+
+        public Order GetOrderByUniqueId(string uniqueId)
+        {
+            var allOrders = (IEnumerable<Order>)orders.Values;
+            return allOrders.Where(x => x.UniqueId == uniqueId).FirstOrDefault();
+        }
+
+        public IEnumerable<Order> GetOrders()
+        {
+            return orders.Values;
+        }
+
+        public IEnumerable<Order> GetOutstandingOrders()
+        {
+            var allOrders = (IEnumerable<Order>)orders.Values;
+            return allOrders.Where(x => x.DateProcessed.HasValue == false);
+        }
+
+        public IEnumerable<Order> GetProcessedOrders()
+        {
+            var allOrders = (IEnumerable<Order>)orders.Values;
+            return allOrders.Where(x => x.DateProcessed.HasValue);
+        }
+
+        public void UpdateOrder(Order order)
+        {
+            if (order == null || !order.OrderId.HasValue) return;
+
+            var ord = orders[order.OrderId.Value];
+            if (ord == null) return;
+            orders[order.OrderId.Value] = order;
+        }
+    }
+}
