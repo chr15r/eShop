804d67768ccec561ddd150067245481a350e84c4
Chris Roche <chris@wetrack.com>
2020-12-26 13:51:04 +0000

    Admin portal setup

---
+++ eShop.UseCases/AdminPortal/OutstandingOrdersScreen/ViewOutstandingOrdersUseCase.cs
@@ -0,0 +1,24 @@
+﻿using eShop.CoreBusiness.Models;
+using eShop.UseCases.PluginInterfaces.DataStore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace eShop.UseCases.AdminPortal.OutstandingOrdersScreen
+{
+    public class ViewOutstandingOrdersUseCase : IViewOutstandingOrdersUseCase
+    {
+        private readonly IOrderRepository orderRepository;
+
+        public ViewOutstandingOrdersUseCase(IOrderRepository orderRepository)
+        {
+            this.orderRepository = orderRepository;
+        }
+
+        public IEnumerable<Order> Execute()
+        {
+            return orderRepository.GetOutstandingOrders();
+        }
+
+    }
+}
