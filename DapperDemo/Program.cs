59e0eb6c434b6439265f1c4f89d44a40cf64a653
Chris Roche <chris@wetrack.com>
2020-12-28 14:20:36 +0000

    Dapper Demo setup

---
+++ DapperDemo/Program.cs
@@ -0,0 +1,24 @@
+﻿using Dapper;
+using eShop.CoreBusiness.Models;
+using System;
+using System.Data;
+using System.Data.SqlClient;
+
+namespace DapperDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var conenctionString = "Data Source=LAPTOP-9CBKDG1T\\SQLEXPRESS;Initial Catalog=eShop;Integrated Security=True";
+            using (IDbConnection connection = new SqlConnection(conenctionString))
+            {
+                var result = connection.Query<Product>("SELECT * FROM Product");
+                foreach (var record in result)
+                {
+                    Console.WriteLine($"{record.Name}: {record.Price}");
+                }
+            }
+        }
+    }
+}
