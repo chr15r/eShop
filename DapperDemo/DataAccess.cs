0e6335d2095feeccc2f2bd38e26801e73c2438c1
Chris Roche <chris@wetrack.com>
2020-12-28 14:54:46 +0000

    Demo wrapper classes for Dapper

---
+++ DapperDemo/DataAccess.cs
@@ -0,0 +1,53 @@
+﻿using Dapper;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+
+namespace DapperDemo
+{
+    public class DataAccess
+    {
+        private readonly string connectionString;
+
+        public DataAccess(string connectionString)
+        {
+            this.connectionString = connectionString;
+        }
+
+        public T QuerySingle<T, U>(string sql, U parameters)
+        {
+            using (IDbConnection connection = new SqlConnection(connectionString))
+            {
+                return connection.QuerySingle<T>(sql, parameters);
+            }
+        }
+
+        public T QueryFirst<T, U>(string sql, U parameters)
+        {
+            using (IDbConnection connection = new SqlConnection(connectionString))
+            {
+                return connection.QueryFirst<T>(sql, parameters);
+            }
+        }
+
+        public List<T> Query<T, U>(string sql, U parameters)
+        {
+            using (IDbConnection connection = new SqlConnection(connectionString))
+            {
+                return connection.Query<T>(sql, parameters).ToList();
+            }
+        }
+
+        public void ExecuteCommand<T>(string sql, T parameters)
+        {
+            using (IDbConnection connection = new SqlConnection(connectionString))
+            {
+                connection.Execute(sql, parameters);
+            }
+        }
+    }
+
+}
--- DapperDemo/Program.cs
+++ DapperDemo/Program.cs
@@ -10,44 +10,62 @@ namespace DapperDemo
     {
         static void Main(string[] args)
         {
-            var conenctionString = "Data Source=LAPTOP-9CBKDG1T\\SQLEXPRESS;Initial Catalog=eShop;Integrated Security=True";
-            using (IDbConnection connection = new SqlConnection(conenctionString))
-            {
+            var connectionString = "Data Source=LAPTOP-9CBKDG1T\\SQLEXPRESS;Initial Catalog=eShop;Integrated Security=True";
+            var da = new DataAccess(connectionString);
 
-                //// CREATE
-                //var sql = @"INSERT INTO Product (ProductId, Brand, Name, Price)  
-                //            VALUES (@ProductId, @Brand, @Name, @Price)";
+            var products1 = da.Query<Product, dynamic>("SELECT * FROM Product", new { });
 
-                //var product = new Product { ProductId = 100000002, Brand = "New Brand", Name = "New Name", Price = 10 };
-                //connection.Execute(sql, product);
+            var products2 = da.QuerySingle<Product, dynamic>("SELECT * FROM Product WHERE Name = @Name", new { Name = "Maybelline Fit Me Blush" });
 
-
-                // UPDATE
-                //var sqlUpdate = @"UPDATE Product 
-                //                    SET ImageLink=@Url 
-                //                    WHERE Name=@Name";
-
-                //connection.Execute(sqlUpdate, new
-                //{
-                //    Url = "https://d3t32hsnjxo7q6.cloudfront.net/i/029889b345c76a70e8bb978b73ed1a87_ra,w158,h184_pa,w158,h184.png",
-                //    Name = "New Name"
-                //});
+            da.ExecuteCommand<Product>(@"INSERT INTO Product (ProductId, Brand, Name, Price)  
+                                            VALUES (@ProductId, @Brand, @Name, @Price)",
+                                 new Product { ProductId = 1000000055, Brand = "New Brand", Name = "New Name", Price = 10 });
 
 
-                // DELETE
-                var sqlDelete = "DELETE FROM Product WHERE Name = @Name";
-                connection.Execute(sqlDelete, new { Name = "New Name" });
+            da.ExecuteCommand<dynamic>(@"UPDATE Product SET ImageLink=@Url WHERE Name=@Name",
+                                 new
+                                 {
+                                     Url = "https://d3t32hsnjxo7q6.cloudfront.net/i/029889b345c76a70e8bb978b73ed1a87_ra,w158,h184_pa,w158,h184.png",
+                                     Name = "New Name"
+                                 });
+
+            //using (IDbConnection connection = new SqlConnection(connectionString))
+            //{
+
+            //    //// CREATE
+            //    //var sql = @"INSERT INTO Product (ProductId, Brand, Name, Price)  
+            //    //            VALUES (@ProductId, @Brand, @Name, @Price)";
+
+            //    //var product = new Product { ProductId = 100000002, Brand = "New Brand", Name = "New Name", Price = 10 };
+            //    //connection.Execute(sql, product);
 
 
-                // READ
-                    var result = connection.Query<Product>("SELECT * FROM Product");
-                foreach (var record in result)
-                {
-                    Console.WriteLine($"{record.Name}: {record.Price}");
-                }
+            //    // UPDATE
+            //    //var sqlUpdate = @"UPDATE Product 
+            //    //                    SET ImageLink=@Url 
+            //    //                    WHERE Name=@Name";
+
+            //    //connection.Execute(sqlUpdate, new
+            //    //{
+            //    //    Url = "https://d3t32hsnjxo7q6.cloudfront.net/i/029889b345c76a70e8bb978b73ed1a87_ra,w158,h184_pa,w158,h184.png",
+            //    //    Name = "New Name"
+            //    //});
 
 
-            }
+            //    // DELETE
+            //    var sqlDelete = "DELETE FROM Product WHERE Name = @Name";
+            //    connection.Execute(sqlDelete, new { Name = "New Name" });
+
+
+            //    // READ
+            //        var result = connection.Query<Product>("SELECT * FROM Product");
+            //    foreach (var record in result)
+            //    {
+            //        Console.WriteLine($"{record.Name}: {record.Price}");
+            //    }
+
+
+            //}
         }
     }
 }
